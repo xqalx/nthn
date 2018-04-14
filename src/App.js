748d2ba82eb6bbaa8055cacd07069f88541ef369
xqalx <jakub.01@wp.pl>
2018-04-14 23:21:21 +0200

    Oy playlisty

--- src/App.js
+++ src/App.js
@@ -69,14 +69,15 @@ class Filter extends Component{
 
 class Playlist extends Component{
   render(){
+    let playlist = this.props.playlist
     return(
       <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
         <img/>
-        <h3>Top Songs</h3>
+        <h3>{playlist.name}</h3>
         <ul>
-          <li>Song 1</li>
-          <li>Song 2</li>
-          <li>Song 3</li>
+          {playlist.songs.map(song =>
+            <li>{song.name}</li>
+          )}
         </ul>
       </div>
     );
@@ -103,10 +104,9 @@ class App extends Component {
           <LiczbaPlaylist playlists={this.state.serverData.user.playlists}/>
           <Godziny playlists={this.state.serverData.user.playlists}/>      
           <Filter/>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>     
+          {this.state.serverData.user.playlists.map(playlist =>
+            <Playlist playlist={playlist}/> 
+          )} 
         </div> : <h1 style={defaultStyle}>Loadnig...</h1>
         }
       </div>
