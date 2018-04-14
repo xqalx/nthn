1582dfe71ce702876545e407b391eb3757226ab5
xqalx <jakub.01@wp.pl>
2018-04-14 22:48:02 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 
 let defaultStyle = {
-  color: '#008EDC',
+  color: 'yellow',
 }
 
-class Bitcoin extends Component{
+let fakeServerData = {
+  user: {
+    name: 'Kuba',
+    playlists: [
+      {
+        name: 'Boi Best',
+        songs: [{name: 'Campfire', duration: 2150}, {name: 'Kryptowaluty',duration: 4340}, {name: 'Gummy', duration: 4200}, {name: 'REDMERCEDES', duration: 2490}]
+      },
+      {
+        name: 'Discover Weekly',
+        songs: [{name: 'Campfire', duration: 2150}, {name: 'Kryptowaluty',duration: 4340}, {name: 'Gummy', duration: 4200}, {name: 'REDMERCEDES', duration: 2490}]
+      },
+      {
+        name: 'Poczekalnia',
+        songs: [{name: 'Campfire', duration: 2150}, {name: 'Kryptowaluty',duration: 4340}, {name: 'Gummy', duration: 4200}, {name: 'REDMERCEDES', duration: 2490}]
+      },
+      {
+        name: 'Canzoni Fresche',
+        songs: [{name: 'Campfire', duration: 2150}, {name: 'Kryptowaluty',duration: 4340}, {name: 'Gummy', duration: 4200}, {name: 'REDMERCEDES', duration: 2490}]
+      },
+  ]
+  }
+}
+
+class LiczbaPlaylist extends Component{
   render(){
     return(
       <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
-        <h2>Kryptowaluty</h2>
+        <h2>{this.props.playlists.length} playlisty</h2>
+      </div>
+    );
+  }
+}
+
+class Godziny extends Component{
+  render(){
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    }, [])
+   let totalDuration = allSongs.reduce((sum, eachSong) => {
+     return sum + eachSong.duration
+   }, 0)
+    
+    return(
+      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+        <h2>{Math.round(totalDuration/60)} godzin</h2>
       </div>
     );
   }
@@ -43,17 +84,31 @@ class Playlist extends Component{
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {serverData: {}}
+  }
+  componentDidMount(){
+    setTimeout(() =>{
+    this.setState({serverData: fakeServerData})
+    }, 1000);
+  }
   render() {
     return (
       <div className="App">
-        <h1 style={{...defaultStyle, 'font-size': '50px'}}>Title</h1>
-        <Bitcoin/>
-        <Bitcoin/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
+        {this.state.serverData.user ?
+        <div>
+          <h1 style={{...defaultStyle, 'font-size': '50px'}}>
+          {this.state.serverData.user.name}'s Playlists</h1>         
+          <LiczbaPlaylist playlists={this.state.serverData.user.playlists}/>
+          <Godziny playlists={this.state.serverData.user.playlists}/>      
+          <Filter/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>     
+        </div> : <h1 style={defaultStyle}>Loadnig...</h1>
+        }
       </div>
     );
   }
